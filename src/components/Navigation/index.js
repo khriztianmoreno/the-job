9eb96e853e0a91662ac3f75fb0d64db8f7bed7bf
briaenrose <brianith.anm@gmail.com>
2021-04-28 19:31:46 -0500

    Changing classComponents to Functional

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Link } from "react-router-dom";
 import jobs from "../../assets/data/jobs.json";
 
-class Navigation extends Component {
+/* class Navigation extends Component {
   constructor() {
     super();
 
@@ -58,11 +58,11 @@ class Navigation extends Component {
                 <li>
                   <Link to="/jobs/apply/1020">Apply for job</Link>
                 </li>
-                {/* {
+                {/{
                   this.state.isAuth && (
                     <li><Link to="/jobs/create">Post a job</Link></li>
                   )
-                } */}
+                } 
                 <li>
                   <Link to="/jobs/create">Post a job</Link>
                 </li>
@@ -77,5 +77,75 @@ class Navigation extends Component {
     );
   }
 }
+ */
+const Navigation = () => {
+  const [state, setState] = useState({});
+
+  const componentWillMount = () => {
+    verifyLogin();
+  };
+
+  const verifyLogin = () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user && user.name) {
+      this.setState({ isAuth: true });
+    }
+  };
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="pull-left">
+          <Link className="navbar-toggle" to="/" data-toggle="offcanvas">
+            <i className="ti-menu" />
+          </Link>
+          <div className="logo-wrapper">
+            <Link className="logo" to="/">
+              <img src="/img/logo.png" alt="logo" />
+            </Link>
+            <Link className="logo-alt" to="/">
+              <img src="/img/logo-alt.png" alt="logo-alt" />
+            </Link>
+          </div>
+        </div>
+        <div className="pull-right user-login">
+          <Link to="/login" className="btn btn-sm btn-primary">
+            login
+          </Link>
+          or <Link to="/register">register</Link>
+        </div>
+        <ul className="nav-menu">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <a href="/jobs">Position</a>
+            <ul>
+              <li>
+                <Link to="/jobs">Browse jobs</Link>
+              </li>
+              <li>
+                <Link to="/jobs/detail/1020">Job detail</Link>
+              </li>
+              <li>
+                <Link to="/jobs/apply/1020">Apply for job</Link>
+              </li>
+              {/* {
+                  this.state.isAuth && (
+                    <li><Link to="/jobs/create">Post a job</Link></li>
+                  )
+                } */}
+              <li>
+                <Link to="/jobs/create">Post a job</Link>
+              </li>
+              <li>
+                <Link to="/candidates">Candidates</Link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navigation;
