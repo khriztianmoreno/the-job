08eac1faedf487f73acd3ac26b82815ea48c192e
briaenrose <brianith.anm@gmail.com>
2021-05-03 22:26:41 -0500

    Modifying RecentJobs to load dynamic data

--- src/components/RecentJobs/index.js
+++ src/components/RecentJobs/index.js
@@ -1,6 +1,30 @@
 /* eslint-disable */
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import jobs from "../../assets/data/jobs.json";
+
+const classNameType = (name) => {
+  let className = "";
+  switch (name) {
+    case "Full time":
+      className = "label-success";
+      break;
+    case "Part time":
+      className = "label-warning";
+      break;
+    case "Freelance":
+      className = "label-info";
+      break;
+    case "Internship":
+      className = "label-danger";
+      break;
+    default:
+      className = "label-success";
+      break;
+  }
+
+  return className;
+};
 
 const RecentJobs = () => (
   <section>
@@ -11,92 +35,33 @@ const RecentJobs = () => (
       </header>
 
       <div className="row item-blocks-connected">
-
-        <div className="col-xs-12">
-          <a className="item-block" href="/">
-            <header>
-              <img src="./img/logo-google.jpg" alt="" />
-              <div className="hgroup">
-                <h4>Senior front-end developer</h4>
-                <h5>Google</h5>
-              </div>
-              <div className="header-meta">
-                <span className="location">Menlo park, CA</span>
-                <span className="label label-success">Full-time</span>
-              </div>
-            </header>
-          </a>
-        </div>
-
-        <div className="col-xs-12">
-          <a className="item-block" href="/">
-            <header>
-              <img src="./img/logo-linkedin.png" alt="" />
-              <div className="hgroup">
-                <h4>Software Engineer (Entry or Senior)</h4>
-                <h5>Linkedin</h5>
-              </div>
-              <div className="header-meta">
-                <span className="location">Livermore, CA</span>
-                <span className="label label-warning">Part-time</span>
-              </div>
-            </header>
-          </a>
-        </div>
-
-        <div className="col-xs-12">
-          <a className="item-block" href="/">
-            <header>
-              <img src="./img/logo-envato.png" alt="" />
-              <div className="hgroup">
-                <h4>Full Stack Web Developer</h4>
-                <h5>Envato</h5>
-              </div>
-              <div className="header-meta">
-                <span className="location">San Francisco, CA</span>
-                <span className="label label-info">Freelance</span>
-              </div>
-            </header>
-          </a>
-        </div>
-
-        <div className="col-xs-12">
-          <a className="item-block" href="/">
-            <header>
-              <img src="./img/logo-facebook.png" alt="" />
-              <div className="hgroup">
-                <h4>Web Applications Developer</h4>
-                <h5>Facebook</h5>
-              </div>
-              <div className="header-meta">
-                <span className="location">Lexington, MA</span>
-                <span className="label label-danger">Internship</span>
-              </div>
-            </header>
-          </a>
-        </div>
-
-        <div className="col-xs-12">
-          <a className="item-block" href="/">
-            <header>
-              <img src="/img/logo-microsoft.jpg" alt="" />
-              <div className="hgroup">
-                <h4>Sr. SQL Server Developer</h4>
-                <h5>Microsoft</h5>
-              </div>
-              <div className="header-meta">
-                <span className="location">Palo Alto, CA</span>
-                <span className="label label-success">Remote</span>
-              </div>
-            </header>
-          </a>
-        </div>
-
+        {jobs.map((job) => (
+          <div key={job.id} className="col-xs-12">
+            <a className="item-block" href="/">
+              <header>
+                <img src={job.image} alt={job.title} />
+                <div className="hgroup">
+                  <h4>{job.title} </h4>
+                  <h5>{job.company} </h5>
+                </div>
+                <div className="header-meta">
+                  <span className="location">{job.location}</span>
+                  <span className={`label ${classNameType(job.type)}`}>
+                    {job.type}
+                  </span>
+                </div>
+              </header>
+            </a>
+          </div>
+        ))}
       </div>
 
-      <br /><br />
+      <br />
+      <br />
       <p className="text-center">
-        <Link className="btn btn-info" to="/jobs">Browse all jobs</Link>
+        <Link className="btn btn-info" to="/jobs">
+          Browse all jobs
+        </Link>
       </p>
     </div>
   </section>
