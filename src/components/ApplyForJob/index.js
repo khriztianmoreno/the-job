386027a122f8554a107306a9a8b2b47356a04099
khriztianmoreno <khriztianmoreno@gmail.com>
2021-05-04 19:38:07 -0500

    Load job detail from service

--- src/components/ApplyForJob/index.js
+++ src/components/ApplyForJob/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import Header from './Header';
 import Main from './Form';
 
-const ApplyJob = () => (
-  <div>
-    <Header />
-    <Main />
-  </div>
-);
+import { getJob } from '../../services/jobs.service';
+
+const ApplyJob = () => {
+  const [jobDetail, setJobDetail] = useState(null);
+  const { id } = useParams();
+
+  const getJobDetail = async () => {
+    const job = await getJob(id);
+    setJobDetail(job);
+  };
+
+  useEffect(() => {
+    getJobDetail();
+  }, [id]);
+
+  return (
+    <div>
+      {jobDetail ? <Header job={jobDetail} /> : null}
+
+      <Main />
+    </div>
+  );
+};
 
 export default ApplyJob;
